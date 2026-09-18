d07a9aef06e1010655332d4c8dda2660b5accff1
Grigor Safaryan <grigor.safaryan@ekeng.am>
2026-09-18 17:35:06 +0400

    feat: config,cookie-parser,env

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { IS_DEV_ENV } from './lib/common/utils/is-dev.util';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EventsModule } from './events/events.module';
@@ -9,7 +10,9 @@ import { PrismaModule } from './prisma/prisma.module';
 @Module({
   imports: [
     ConfigModule.forRoot({
+      ignoreEnvFile: !IS_DEV_ENV,
       isGlobal: true,
+      envFilePath: '.env',
     }),
     PrismaModule,
     EventsModule,
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,29 @@
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.SERVER_PORT ?? 3000);
+
+  const config = app.get(ConfigService);
+
+  app.use(cookieParser(config.getOrThrow('COOKIES_SECRET')));
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+    }),
+  );
+
+  app.enableCors({
+    origin: config.getOrThrow<string>('ALLOWED_ORIGIN'),
+    credentials: true,
+    exposedHeaders: ['set-cookie'],
+  });
+
+  // await app.listen(process.env.SERVER_PORT ?? 3000);
+  await app.listen(config.getOrThrow<number>('SERVER_PORT'));
 }
 bootstrap();
